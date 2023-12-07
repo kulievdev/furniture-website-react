d427470b2017a5d06cec2e1fdcf054dfcfb72ee2
Tohir Kuliev <kuliev.tech@gmail.com>
2023-12-07 12:22:23 -0500

    implement hero-component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,11 @@
 import { TopNavigation } from "./sections/top-navigation/TopNavigation";
+import { Hero } from "./sections/hero/Hero";
 
 const Home = () => {
     return (
         <>
             <TopNavigation />
+            <Hero />
         </>
     );
 };
