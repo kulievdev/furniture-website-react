7e2a56c0ba29951c20d8aa7071d06c5721781561
Tohir Kuliev <kuliev.tech@gmail.com>
2024-03-26 06:51:29 -0400

    Add more sizes

--- src/pages/home/sections/top-navigation/TopNavigation.js
+++ src/pages/home/sections/top-navigation/TopNavigation.js
@@ -1,42 +1,53 @@
+import styled from "styled-components";
 import Logo from "../../../../design-system/Logo";
-import "./TopNavigation.css";
 
 const links = [
     { text: "Products", link: "" },
     { text: "Rooms", link: "" },
     { text: "Inspiration", link: "" },
-    { text: "Support", link: "" },
-    { text: "Sign in", link: "" }
+    { text: "Support", link: "" }
 ];
+
+const Header = styled.header``;
+const NavContainer = styled.div`
+    max-width: 160rem;
+    padding: 4rem 15rem;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+const Navigation = styled.nav``;
+const UnorderedList = styled.ul`
+    display: flex;
+    gap: var(--space-50);
+`;
+const List = styled.li``;
+const Link = styled.a`
+    font-size: var(--space-17);
+    color: var(--dark-navy);
+    font-weight: 600;
+    line-height: var(--space-30);
+`;
+const NavigationAction = styled(Link)``;
 
 const TopNavigation = () => {
     return (
-        <header>
-            <div className="header-content container">
+        <Header>
+            <NavContainer>
                 <Logo />
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="#">Products</a>
-                        </li>
-                        <li>
-                            <a href="#">Rooms</a>
-                        </li>
-                        <li>
-                            <a href="#">Inspiration</a>
-                        </li>
-                        <li>
-                            <a href="#">Support</a>
-                        </li>
-                    </ul>
-                </nav>
-                <span>
-                    <a className="sign-in" href="#">
-                        Sign in
-                    </a>
-                </span>
-            </div>
-        </header>
+                <Navigation>
+                    <UnorderedList>
+                        {links.map((link, idx) => (
+                            <List key={idx}>
+                                <Link href={link.link}>{link.text}</Link>
+                            </List>
+                        ))}
+                    </UnorderedList>
+                </Navigation>
+                <NavigationAction>Sign in</NavigationAction>
+            </NavContainer>
+        </Header>
     );
 };
 
