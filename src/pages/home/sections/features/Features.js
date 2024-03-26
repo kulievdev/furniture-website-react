79ed862d7356f519f02932446c7f60bb5189e046
Tohir Kuliev <kuliev.tech@gmail.com>
2024-03-26 11:05:02 -0400

    include dashed line

--- src/pages/home/sections/features/Features.js
+++ src/pages/home/sections/features/Features.js
@@ -10,35 +10,47 @@ const FeaturesSectionContainer = styled.div`
     max-width: 160rem;
     margin: 0 auto;
     padding: 10rem 12rem;
+`;
+
+const FeaturesWrapper = styled.div`
     display: flex;
     gap: var(--space-60);
     justify-content: space-between;
+    margin-bottom: var(--space-80);
+`;
+
+const DashedLine = styled.div`
+    border: var(--space-2) dashed #333;
+    opacity: 0.17;
 `;
 
 const Features = () => {
     return (
         <FeaturesSection id="features">
             <FeaturesSectionContainer>
-                <Feature
-                    imgSrc={heartImg}
-                    heading="Best Quality"
-                    description="Uncompromising excellence delivered in every product and experience."
-                    backgroundColor="var(--light-green)"
-                />
+                <FeaturesWrapper>
+                    <Feature
+                        imgSrc={heartImg}
+                        heading="Best Quality"
+                        description="Uncompromising excellence delivered in every product and experience."
+                        backgroundColor="var(--light-green)"
+                    />
 
-                <Feature
-                    imgSrc={zapImg}
-                    heading="Fastest Delivery"
-                    description="Swift and efficient shipping to bring your order to you promptly."
-                    backgroundColor="var(--baby-pink)"
-                />
+                    <Feature
+                        imgSrc={zapImg}
+                        heading="Fastest Delivery"
+                        description="Swift and efficient shipping to bring your order to you promptly."
+                        backgroundColor="var(--baby-pink)"
+                    />
 
-                <Feature
-                    imgSrc={headphonesImg}
-                    heading="Great Support"
-                    description="Exceptional customer service to assist and guide you effectively."
-                    backgroundColor="var(--light-blue)"
-                />
+                    <Feature
+                        imgSrc={headphonesImg}
+                        heading="Great Support"
+                        description="Exceptional customer service to assist and guide you effectively."
+                        backgroundColor="var(--light-blue)"
+                    />
+                </FeaturesWrapper>
+                <DashedLine />
             </FeaturesSectionContainer>
         </FeaturesSection>
     );
