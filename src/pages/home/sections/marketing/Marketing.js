c9eb4ec7c84bd9f766e5b3a38c351cd3db6f6b63
Tohir Kuliev <kuliev.tech@gmail.com>
2024-03-27 08:59:17 -0400

    Make Marketing section fully responsive

--- src/pages/home/sections/marketing/Marketing.js
+++ src/pages/home/sections/marketing/Marketing.js
@@ -9,17 +9,32 @@ const MarketingSection = styled.section`
 const MarketingSectionContainer = styled.div`
     max-width: 160rem;
     margin: 0 auto;
-    padding: 10rem 15rem;
-    display: flex;
     gap: 16rem;
+    padding: 10rem 2rem;
+
+    @media (min-width: 500px) {
+        padding: 10rem 5rem;
+    }
+
+    @media (min-width: 800px) {
+        padding: 10rem 15rem;
+    }
+
+    @media (min-width: 1100px) {
+        display: flex;
+    }
 `;
 
 const MarketingImageWrapper = styled.div`
-    width: 50%;
+    width: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
-    height: 80rem;
+    margin-bottom: var(--space-60);
+
+    @media (min-width: 1100px) {
+        width: 50%;
+    }
 `;
 
 const MarketingImage = styled.img`
@@ -29,17 +44,21 @@ const MarketingImage = styled.img`
 `;
 
 const MarketingContent = styled.div`
-    width: 50%;
+    width: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
+
+    @media (min-width: 1100px) {
+        width: 50%;
+    }
 `;
 
 const MarketingHeading = styled.h2`
     color: var(--dark-navy);
     font-size: var(--space-42);
-    font-weight: 600;
     line-height: var(--space-50);
+    font-weight: 600;
     letter-spacing: -0.1rem;
     margin-bottom: var(--space-18);
 `;
@@ -47,8 +66,8 @@ const MarketingHeading = styled.h2`
 const MarketingDescription = styled.p`
     color: var(--dark-navy);
     font-size: var(--space-18);
-    font-weight: 400;
     line-height: var(--space-30);
+    font-weight: 400;
     opacity: 0.6;
     margin-bottom: var(--space-46);
 `;
@@ -62,9 +81,9 @@ const MarketingCtaWrapper = styled.div`
 const MarketingCtaText = styled.p`
     color: var(--dark-navy);
     font-size: var(--space-14);
-    font-weight: 600;
     line-height: var(--space-22);
     letter-spacing: 0.4rem;
+    font-weight: 600;
     text-transform: uppercase;
 `;
 
